5b484ce9b1697114e76d30d0be0deb8386cc00b6
rjivraj72-ops <rjivraj72@gmail.com>
2026-07-05 20:47:18 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function loadPolishedUi() {
   }
   const polishLink = document.createElement("link");
   polishLink.rel = "stylesheet";
-  polishLink.href = "polish.css?v=20260705-admin-desktop-polish";
+  polishLink.href = "polish.css?v=20260705-toggle-polish";
   polishLink.dataset.uiPolish = "production";
   document.head.appendChild(polishLink);
 }
